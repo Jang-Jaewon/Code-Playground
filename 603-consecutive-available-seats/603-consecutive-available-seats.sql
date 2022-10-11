e54807e1eaa9a5c3667716cd9ae3b2334159d9be
Jang Jaewon <jewon119@gmail.com>
2022-10-11 09:04:19 +0900

    Time: 683 ms (19.72%), Space: 0B (100.00%) - LeetHub

---
+++ 603-consecutive-available-seats/603-consecutive-available-seats.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT seat_id
+FROM cinema
+WHERE free = 1 AND (
+    seat_id - 1 IN (SELECT seat_id FROM cinema WHERE free = 1) OR
+    seat_id + 1 IN (SELECT seat_id FROM cinema WHERE free = 1)
+);
