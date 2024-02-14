7c85c45df0445a24d30515336b3f3836ee9a4502
Jang Jaewon <jewon119@gmail.com>
2024-02-14 21:04:31 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9544 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze I/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+l = [0] * 10001
+for i in range(n):
+    data = int(sys.stdin.readline())
+    l[data] += 1
+for i in range(10001):
+    if l[i] != 0:
+        for j in range(l[i]):
+            print(i)
